e132f2ce8a9b20203479eaf60dcec85ce7b539b6
santoss2009 <santos.gabi.tmz@gmail.com>
2025-08-12 13:09:47 +0000

    concluido

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Matemática',
+    'O que é Matemática?',
+    'é uma ciência que estuda números, formas, estruturas, quantidades e suas relações '
+)
+
+criaCartao(
+    'Matemática',
+    'A matemática é usada no seu dia a dia?'
+    'Sim,a matemática é usada no dia a dia de diversas formas.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é uma função?',
+    'é uma relação entre dois conjuntos, onde cada elemento do primeiro conjunto (o domínio) está associado a um único elemento'
+
+criaCartao(
+    'Matemática',
+    'Qual é o resultado da expressão (10 + 5) * 2 - 8 / 2 ??',
+    '30−4=26'
+)
